2b929edb9e6f47a8de277d8e2e36406573ae9901
meowbilli <157163811+meowbilli@users.noreply.github.com>
2025-07-06 20:21:25 +0530

    Update run.py

--- day1/run.py
+++ day1/run.py
@@ -1,5 +1,5 @@
 import os
 from flaskblog import app
 
-port = int(os.environ.get('PORT', 5000))  # use Railway's PORT if available
+port = int(os.environ.get('PORT', 8080))  # use Railway's PORT if available
 app.run(host='0.0.0.0', port=port)
